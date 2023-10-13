fa38f7c40d9ff910327ac81e0ab25b5e4c57d835
Fabrizio Ortiz <fabrizioortizo.main@gmail.com>
2023-10-12 22:40:53 -0500

    feat(project): :art: add Tailwind Styles to main, container, texts, basics

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-    <div className='text-[3.125rem] text-red-400'>
-      Tailwind
-    </div>
-    </>
-  )
+    <main className="w-full h-screen flex justify-center items-center bg-dark-blue">
+      <div className="relative max-w-[33.7rem] p-[3rem] m-[1.3rem] mb-[4rem] bg-dark-grayish-blue text-center">
+        <h1 className="text-[.8rem] text-neon-green">ADVICE #166</h1>
+        <h2 className="text-[1.7rem] text-light-cyan">“The quieter you become, the more you can hear.”</h2>
+        <div>----</div>
+
+        <button className="absolute bottom-[-2rem] left-[50%] translate-x-[-50%] w-[4rem] h-[4rem] bg-neon-green rounded-full">
+          x
+        </button>
+      </div>
+    </main>
+  );
 }
 
-export default App
+export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,22 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "light-cyan": "hsl(193, 38%, 86%)",
+        "neon-green": "hsl(150, 100%, 66%)",
+        "grayish-blue": "hsl(217, 19%, 38%)",
+        "dark-grayish-blue": "hsl(217, 19%, 24%)",
+        "dark-blue": "hsl(218, 23%, 16%)",
+      },
+      backgroundColor: {
+        "light-cyan": "hsl(193, 38%, 86%)",
+        "neon-green": "hsl(150, 100%, 66%)",
+        "grayish-blue": "hsl(217, 19%, 38%)",
+        "dark-grayish-blue": "hsl(217, 19%, 24%)",
+        "dark-blue": "hsl(218, 23%, 16%)",
+      }
+    },
   },
   plugins: [],
 }
