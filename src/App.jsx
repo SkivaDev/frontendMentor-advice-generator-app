20489b70966191fcb3e4ef47a8a58e30e8fd140a
Fabrizio Ortiz <fabrizioortizo.main@gmail.com>
2023-10-14 15:24:53 -0500

    add loading effect Tailwind CSS

--- src/App.jsx
+++ src/App.jsx
@@ -8,12 +8,14 @@ import adviceslipApi from "./libs/adviceslipApi";
 
 function App() {
   const [advice, setAdvice] = useState("");
+  const [adviceId, setAdviceId] = useState(0);
   const [isLoading, setIsLoading] = useState(false);
 
   function getAdvice() {
     setIsLoading(true);
     adviceslipApi.getRandomAdvice({}).then((data) => {
-      console.log(data.data.slip.advice);
+      console.log(data.data);
+      setAdviceId(data.data.slip.id)
       setAdvice(data.data.slip.advice);
       setIsLoading(false);
     });
@@ -29,14 +31,14 @@ function App() {
 
   return (
     <main className="w-full h-screen flex justify-center items-center bg-dark-blue">
-      <div className="relative max-w-[33.7rem] p-[3rem] pb-[4rem] m-[1.3rem] mb-[4rem] bg-dark-grayish-blue text-center rounded-xl">
+      <div className="relative max-w-[33.7rem] p-[3rem] pb-[4rem] m-[1.3rem] mb-[4rem] bg-dark-grayish-blue text-center rounded-xl transition-all">
         <h1 className="sr-only">Advice generator</h1>
-        <div className={`loader z-10 ${isLoading ? '' : 'invisible'}`} role="alert" aria-live="assertive">
+        <div className={`loader z-10 ${isLoading ? '' : 'hidden'}`} role="alert" aria-live="assertive">
             <p className="sr-only loader__spinner">Loading...</p>
           </div>
-        <div className={` ${isLoading ? 'invisible' : ''}`}>
+        <div className={`transition-all ${isLoading ? 'invisible' : ''}`}>
           <h2 className="text-[.8rem] text-neon-green mb-[24px] tracking-[0.3rem]">
-            ADVICE #166
+            ADVICE #{adviceId}
           </h2>
           <p className="text-[1.7rem] text-light-cyan mb-[40px]">“{advice}”</p>
           <picture className="mb-[.75rem]">
@@ -45,7 +47,7 @@ function App() {
           </picture>
         </div>
 
-        <button className="btn absolute bottom-[-2rem] left-[50%] translate-x-[-50%] w-[4rem] h-[4rem] bg-neon-green rounded-full flex justify-center items-center z-10">
+        <button className={`btn absolute bottom-[-2rem] left-[50%] translate-x-[-50%] w-[4rem] h-[4rem] bg-neon-green rounded-full flex justify-center items-center z-10 ${isLoading ? 'hidden' : ''}`}>
           <img src={dice} alt="dice" onClick={handleClick} />
         </button>
       </div>
